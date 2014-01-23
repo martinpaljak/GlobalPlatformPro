f944a0e9d4c1bfbbf68a8bfe5434ddaa7d619507
Martin Paljak <martin@martinpaljak.net>
2014-01-23 23:53:15 +0000

    Use stdout instead of stderr

--- src/openkms/gpj/LoggingCardTerminal.java
+++ src/openkms/gpj/LoggingCardTerminal.java
@@ -16,15 +16,15 @@ public class LoggingCardTerminal extends CardTerminal {
 	private static final char[] LOWER_HEX = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 	public static String encodeHexString(final byte[] data) {
 
-        final int l = data.length;
-        final char[] out = new char[l << 1];
-        // two characters form the hex value.
-        for (int i = 0, j = 0; i < l; i++) {
-            out[j++] = LOWER_HEX[(0xF0 & data[i]) >>> 4];
-            out[j++] = LOWER_HEX[0x0F & data[i]];
-        }
-        return new String(out);
-    }
+		final int l = data.length;
+		final char[] out = new char[l << 1];
+		// two characters form the hex value.
+		for (int i = 0, j = 0; i < l; i++) {
+			out[j++] = LOWER_HEX[(0xF0 & data[i]) >>> 4];
+			out[j++] = LOWER_HEX[0x0F & data[i]];
+		}
+		return new String(out);
+	}
 	// End of copied code from commons-codec
 
 	protected static CardTerminal terminal = null;
@@ -46,8 +46,8 @@ public class LoggingCardTerminal extends CardTerminal {
 
 	@Override
 	public Card connect(String arg0) throws CardException {
-		System.err.format("SCardConnect(%s)\n", arg0);
-		System.err.flush();
+		System.out.format("SCardConnect(%s)\n", arg0);
+		System.out.flush();
 		return new LoggingCard(terminal, arg0);
 	}
 
@@ -81,22 +81,22 @@ public class LoggingCardTerminal extends CardTerminal {
 
 		@Override
 		public void beginExclusive() throws CardException {
-			System.err.println("SCardBeginTransaction()");
-			System.err.flush();
+			System.out.println("SCardBeginTransaction()");
+			System.out.flush();
 			card.beginExclusive();
 		}
 
 		@Override
 		public void disconnect(boolean arg0) throws CardException {
-			System.err.format("SCardDisconnect(%b)\n", arg0);
-			System.err.flush();
+			System.out.format("SCardDisconnect(%b)\n", arg0);
+			System.out.flush();
 			card.disconnect(arg0);
 		}
 
 		@Override
 		public void endExclusive() throws CardException {
-			System.err.println("SCardEndTransaction()");
-			System.err.flush();
+			System.out.println("SCardEndTransaction()");
+			System.out.flush();
 			card.endExclusive();
 		}
 
@@ -151,27 +151,31 @@ public class LoggingCardTerminal extends CardTerminal {
 			public ResponseAPDU transmit(CommandAPDU apdu) throws CardException {
 
 				int len = apdu.getData().length > 255 ? 7 : 5;
-				System.err.print("A>> " + card.getProtocol() + " (4+" + String.format("%04d", apdu.getData().length) + ")");
-				System.err.print(" " + encodeHexString(Arrays.copyOfRange(apdu.getBytes(), 0, 4)));
+				System.out.print("A>> " + card.getProtocol() + " (4+" + String.format("%04d", apdu.getData().length) + ")");
+				System.out.print(" " + encodeHexString(Arrays.copyOfRange(apdu.getBytes(), 0, 4)));
 
 				// Only if Case 2, 3 or 4 APDU
 				if (apdu.getBytes().length > 4) {
-					System.err.print(" " + encodeHexString(Arrays.copyOfRange(apdu.getBytes(), 4, len)));
-					System.err.println(" " + encodeHexString(Arrays.copyOfRange(apdu.getBytes(), len, apdu.getBytes().length)));
+					System.out.print(" " + encodeHexString(Arrays.copyOfRange(apdu.getBytes(), 4, len)));
+					System.out.println(" " + encodeHexString(Arrays.copyOfRange(apdu.getBytes(), len, apdu.getBytes().length)));
 				} else {
-					System.err.println();
+					System.out.println();
 				}
-				System.err.flush();
-				
+				System.out.flush();
+
 				long t = System.currentTimeMillis();
 				ResponseAPDU response = channel.transmit(apdu);
 				long ms = System.currentTimeMillis() - t;
-				System.err.print("A<< (" + String.format("%04d", response.getData().length) + "+2) (" + ms + "ms)");
-				if (response.getData().length > 2) {
-					System.err.print(" " + encodeHexString(response.getData()));
+				String time = ms + "ms";
+				if (ms > 1000) {
+					time = ms / 1000 + "s" + ms % 1000 + "ms";
 				}
-				System.err.println(" " + encodeHexString(Arrays.copyOfRange(response.getBytes(), response.getBytes().length-2, response.getBytes().length)));
-				System.err.flush();
+				System.out.print("A<< (" + String.format("%04d", response.getData().length) + "+2) (" + time + ")");
+				if (response.getData().length > 2) {
+					System.out.print(" " + encodeHexString(response.getData()));
+				}
+				System.out.println(" " + encodeHexString(Arrays.copyOfRange(response.getBytes(), response.getBytes().length-2, response.getBytes().length)));
+				System.out.flush();
 
 				return response;
 			}
@@ -179,17 +183,17 @@ public class LoggingCardTerminal extends CardTerminal {
 			@Override
 			public int transmit(ByteBuffer cmd, ByteBuffer rsp) throws CardException {
 				byte[] commandBytes = new byte[cmd.remaining()];
-		        cmd.get(commandBytes);
-		        cmd.position(0);
+				cmd.get(commandBytes);
+				cmd.position(0);
 
-				System.err.println("B>> " + card.getProtocol() + " (" + commandBytes.length + ") " + encodeHexString(commandBytes));
-				System.err.flush();
+				System.out.println("B>> " + card.getProtocol() + " (" + commandBytes.length + ") " + encodeHexString(commandBytes));
+				System.out.flush();
 				int response = channel.transmit(cmd, rsp);
 				byte[] responseBytes = new byte[response];
 				rsp.get(responseBytes);
 				rsp.position(0);
-				System.err.println("B<< (" + responseBytes.length + ") " + encodeHexString(responseBytes));
-				System.err.flush();
+				System.out.println("B<< (" + responseBytes.length + ") " + encodeHexString(responseBytes));
+				System.out.flush();
 				return response;
 			}
 		}

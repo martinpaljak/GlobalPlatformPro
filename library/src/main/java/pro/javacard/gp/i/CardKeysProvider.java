8bbbec6654a4983b03fbeda80c9d52f9caea7399
Martin Paljak <martin@martinpaljak.net>
2020-06-17 10:17:46 +0300

    Rename provider

--- library/src/main/java/pro/javacard/gp/i/CardKeysProvider.java
+++ library/src/main/java/pro/javacard/gp/i/CardKeysProvider.java
@@ -4,6 +4,6 @@ import pro.javacard.gp.GPCardKeys;
 
 import java.util.Optional;
 
-public interface CardKeyProvider {
+public interface CardKeysProvider {
     Optional<GPCardKeys> getCardKeys(String spec);
 }

0cea913c768bc966abbb4c5ce7bfa4c44d6bcc22
Martin Paljak <martin@martinpaljak.net>
2023-04-03 18:23:07 +0300

    Snapshot changes for updates

--- library/src/main/java/module-info.java
+++ library/src/main/java/module-info.java
@@ -1,10 +1,12 @@
+// https://stackoverflow.com/a/67895919/44289
+@SuppressWarnings({"requires-automatic"})
 module pro.javacard.globalplatform {
-    requires apdu4j.core;
+    requires transitive apdu4j.core;
     requires ber.tlv;
     requires org.bouncycastle.pkix;
     requires org.bouncycastle.provider; // FIXME: Hex. uses this
     requires org.slf4j;
-    requires pro.javacard.capfile;
+    requires transitive pro.javacard.capfile;
 
     exports pro.javacard.gp;
 }
--- library/src/main/java/pro/javacard/gp/GPKeyInfo.java
+++ library/src/main/java/pro/javacard/gp/GPKeyInfo.java
@@ -218,7 +218,7 @@ public final class GPKeyInfo {
     }
 
 
-    static class GPKeyInfoElement {
+    public static class GPKeyInfoElement {
         final GPKey key;
         final int keyLength;
         final int templateLength;
